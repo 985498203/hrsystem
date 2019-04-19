4a09cd9a22e15860f988aff84eba88ba34896bbb
Administrator <admin@example.com>
2019-04-19 17:19:03 +0800

    2.0.1

--- src/test/java/hrsystem/BaseTest.java
+++ src/test/java/hrsystem/BaseTest.java
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/config/applicationContext-*.xml","classpath:/config/applicationContext-beans-*.xml","classpath:/config/applicationContext-beans.xml"})
 public class BaseTest {
-	
+	 
 }
